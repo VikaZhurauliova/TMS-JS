d4befce49d1ab72c9abc18b7eeae47fe4ebd6687
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-06-13 14:44:43 +0300

    first commit

--- TMS_JS/main.js
+++ TMS_JS/main.js
@@ -1,5 +1,5 @@
-// import * as bootstrap from 'bootstrap';
+import 'bootstrap';
 import * as HOMEWORK from "./HOMEWORK";
-// import './style.css';
+import './style.scss';
 
 // HOMEWORK.VIKA_ZHURAULIOVA_HW_14()
